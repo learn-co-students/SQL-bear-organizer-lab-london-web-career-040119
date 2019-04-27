f4b74a8af5794cd9bb97e5990d7b59976ddb9d32
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-04-27 21:48:23 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr Chocolate", 1, "male", "Brown", "Happy", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "male", "Caramel", "Angry", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 3, "female", "White", "Gentle", 0);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 4, "male", "Brown", "Happy", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 5, "female", "Caramel", "Calm", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 4, "male", "Black", "Grumpy", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 1, "female", "Black", "Happy", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 4, "male", "Brown", "Angry", 0);
